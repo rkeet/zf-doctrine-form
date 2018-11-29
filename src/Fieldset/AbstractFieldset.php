425e7f4102660e9dfc3072b4002d68481578dde9
Robin Keet <robin@keet.me>
2018-11-29 13:13:07 +0100

    Added id field by default to AbstractFieldset and AbstractFieldsetInputFilter.

--- src/Fieldset/AbstractFieldset.php
+++ src/Fieldset/AbstractFieldset.php
@@ -2,6 +2,7 @@
 
 namespace Keet\Form\Fieldset;
 
+use Zend\Form\Element\Hidden;
 use Zend\Form\Fieldset;
 use Zend\I18n\Translator\TranslatorAwareTrait;
 use Zend\InputFilter\InputFilterAwareTrait;
@@ -10,4 +11,18 @@ abstract class AbstractFieldset extends Fieldset
 {
     use InputFilterAwareTrait;
     use TranslatorAwareTrait;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        $this->add(
+            [
+                'name'     => 'id',
+                'type'     => Hidden::class,
+                'required' => false,
+            ]
+        );
+    }
 }
--- src/InputFilter/AbstractFieldsetInputFilter.php
+++ src/InputFilter/AbstractFieldsetInputFilter.php
@@ -2,6 +2,27 @@
 
 namespace Keet\Form\InputFilter;
 
+use Zend\Filter\ToInt;
+use Zend\I18n\Validator\IsInt;
+
 abstract class AbstractFieldsetInputFilter extends AbstractInputFilter
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        $this->add(
+            [
+                'name'       => 'id',
+                'required'   => false,
+                'filters'    => [
+                    ['name' => ToInt::class],
+                ],
+                'validators' => [
+                    ['name' => IsInt::class],
+                ],
+            ]
+        );
+    }
 }
